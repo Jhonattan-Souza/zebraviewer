fc3c86464cfdb8df3c94c3eaf8b36b82fb031a17
Jhonattan Souza <Jhonattan Souza>
2017-05-02 23:51:37 -0300

    Add project files.

---
+++ ZebraViewer/Models/Printer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZebraViewer.Models
+{
+    public class Printer
+    {
+        public string Name { get; set; }
+        public string PortName { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ ZebraViewer/Services/PrinterService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using ZebraViewer.Models;
+
+namespace ZebraViewer.Services
+{
+    public class PrinterService
+    {
+        public static IEnumerable<Printer> GetPrinters()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Printer");
+
+            foreach (ManagementObject printer in searcher.Get())
+            {
+                yield return new Printer
+                {
+                    Name = printer.GetPropertyValue("Name").ToString(),
+                    PortName = printer.GetPropertyValue("PortName").ToString()
+                };
+            }
+        }
+
+        public static void SetPrinterPort(Printer printerToChange, string portPath)
+        {
+            ManagementObjectSearcher searcher = 
+                new ManagementObjectSearcher($"SELECT * FROM Win32_Printer where Name like %{printerToChange.Name}%");
+
+            foreach (ManagementObject printer in searcher.Get())
+            {
+                printer.SetPropertyValue("PortName", portPath);
+            }
+        }
+    }
+}
